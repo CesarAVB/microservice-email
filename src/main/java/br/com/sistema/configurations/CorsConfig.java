363f0384d7ac41f0e79f2b510545b920590dbab4
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-01-10 17:05:30 -0300

    atualizado classe CorsConfig com a URL do Portfolio

--- src/main/java/br/com/sistema/configurations/CorsConfig.java
+++ src/main/java/br/com/sistema/configurations/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**").allowedOrigins("http://localhost:4200", "https://portfolio.cesaraugusto.dev.br/")
+		registry.addMapping("/api/**").allowedOrigins("http://localhost:4200", "https://portfolio.cesaraugusto.dev.br")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
 	}
 	
